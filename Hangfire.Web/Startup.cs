68e469424dd0f204df598bc3bf6b2ac9bf788058
Swagat Swain <swagat@mindfiresolutions.com>
2019-05-30 16:16:49 +0530

    Installs hangfire

---
+++ Hangfire.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.Web.Startup))]
+namespace Hangfire.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            //GlobalConfiguration.Configuration.UseSqlServerStorage("DemoHangfireDB");
+            //app.UseHangfireServer();
+            //app.UseHangfireDashboard();
+        }
+    }
+}
