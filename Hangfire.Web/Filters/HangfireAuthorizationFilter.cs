4f2c5d7c39d2acd111d0ebc32e25be489aafcb47
Swagat Swain <swagat@mindfiresolutions.com>
2019-06-05 08:56:43 +0530

    adds auth filter

---
+++ Hangfire.Web/Filters/HangfireAuthorizationFilter.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.Web.Filters
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            return owinContext.Authentication.User.Identity.IsAuthenticated;
+        }
+    }
+}
--- Hangfire.Web/Startup.cs
+++ Hangfire.Web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using Hangfire.Web.Filters;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(Hangfire.Web.Startup))]
